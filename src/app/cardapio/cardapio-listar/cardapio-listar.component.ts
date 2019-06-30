6e64a926846fc5f2123a87b745c37f7277ca7883
anielen rossini <anielenrossini12@gmail.com>
2019-06-30 16:31:29 -0300

    final

--- src/app/cardapio/cardapio-listar/cardapio-listar.component.ts
+++ src/app/cardapio/cardapio-listar/cardapio-listar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+import { Cardapio } from '../entidade/cardapio';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cardapio-listar',
@@ -7,7 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardapioListarComponent implements OnInit {
 
-  constructor() { }
+  listaCardapio : Observable<Cardapio[]>;
+
+  constructor(private fire: AngularFireDatabase) {
+   this.listaCardapio = this.fire.list<Cardapio>('cardapio')
+   .snapshotChanges().pipe(
+     map( lista => lista.map( linha =>({
+       key: linha.payload.key, ... linha.payload.val()
+     }) ) )
+    );
+  }
 
   ngOnInit() {}
 
